77b59520f26743a22d9fa1e8846599492ab64ad5
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-12 15:19:22 -0300

    environment

---
+++ src/app/configurations/environment.ts
@@ -0,0 +1,7 @@
+const apiUrl = "http://localhost:8080";
+
+export const endpoints = {
+    produto : `${apiUrl}/api/produtos/`,
+    consultar_produtos : `${apiUrl}/api/produtos/consultar`,
+    categoria : `${apiUrl}/api/categorias`
+};
