006ef365f8973cec17522615de577b052dc21ab2
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-21 13:18:24 -0300

    autenticar and criar usuario components

---
+++ src/app/components/pages/autenticar-usuario/autenticar-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autenticar-usuario',
+  imports: [],
+  templateUrl: './autenticar-usuario.component.html',
+  styleUrl: './autenticar-usuario.component.css'
+})
+export class AutenticarUsuarioComponent {
+
+}
---
+++ src/app/components/pages/criar-usuario/criar-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-usuario',
+  imports: [],
+  templateUrl: './criar-usuario.component.html',
+  styleUrl: './criar-usuario.component.css'
+})
+export class CriarUsuarioComponent {
+
+}
