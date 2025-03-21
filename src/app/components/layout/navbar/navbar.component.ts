0adb88f1fdf46734400e31be183baa3eeaf2d12d
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-21 14:12:40 -0300

    adjusted nav bar

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    CommonModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  // Atributos
+  isAuthenticated: boolean = false;
+  nome: string = '';
+  email: string = '';
+  perfil: string = '';
+
+  // Evento executado ao abrir o componente
+  ngOnInit() {
+
+    // Verificar se os dados do usuário estão gravados na session storage
+    if(sessionStorage.getItem('usuario') != null) {
+      this.isAuthenticated = true;
+
+      // Ler os dados do usuário
+      var data = sessionStorage.getItem('usuario') as string;
+      var json = JSON.parse(data);
+
+      this.nome = json.nome;
+      this.email = json.email;
+      this.perfil = json.perfil;
+    }
+  }
+
+  logout() {
+    if(window.confirm('Deseja realmente sair do sistema?')) {
+      sessionStorage.removeItem('usuario');
+      location.href = '/pages/autenticar-usuario';
+    }
+  }
+
 
 }
